2f01bbe1c16dac2bdb47de098d36820d9acf4aa8
Sasha <mazur.sasha.moo@gmail.com>
2018-03-22 23:59:10 +0200

    init base structure of GeneralSettings

---
+++ app/src/main/java/com/example/sasham/goodnews/activity/GeneralSettingsFragment.java
@@ -0,0 +1,26 @@
+package com.example.sasham.goodnews.activity;
+
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.sasham.goodnews.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class GeneralSettingsFragment extends PreferenceFragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.general_settings);
+    }
+
+}
