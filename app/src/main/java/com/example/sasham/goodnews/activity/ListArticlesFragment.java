99dc3aaad00f1e3dc43735dcbbf14455769ea82b
Sasha <mazur.sasha.moo@gmail.com>
2018-04-07 16:02:32 +0300

    Show message if not internet connection

--- app/src/main/java/com/example/sasham/goodnews/activity/ListArticlesFragment.java
+++ app/src/main/java/com/example/sasham/goodnews/activity/ListArticlesFragment.java
@@ -7,9 +7,11 @@ import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.sasham.goodnews.App;
 import com.example.sasham.goodnews.R;
@@ -20,9 +22,12 @@ import com.wang.avi.AVLoadingIndicatorView;
 
 import java.util.List;
 
+import static android.view.View.GONE;
+
 
 public class ListArticlesFragment extends Fragment implements LoaderManager.LoaderCallbacks<Object> {
 
+    private static final String TAG = ListArticlesFragment.class.getSimpleName();
     private RecyclerView mRecyclerView;
     private ArticleAdapter mAdapter;
 
@@ -37,17 +42,21 @@ public class ListArticlesFragment extends Fragment implements LoaderManager.Load
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_list_articles, container, false);
 
+        TextView noConnectionView = (TextView) rootView.findViewById(R.id.tv_no_internet_connection);
         mRecyclerView = (RecyclerView) rootView.findViewById(R.id.top_headlines_recycler_view);
-        mLoadingView=(AVLoadingIndicatorView)rootView.findViewById(R.id.articles_list_loading_view);
+        mLoadingView = (AVLoadingIndicatorView) rootView.findViewById(R.id.articles_list_loading_view);
 
         if (App.isOnline(getContext())) {
             getLoaderManager().initLoader(ARTICLES_LOADER, null, this);
             mLoadingView.show();
+            noConnectionView.setVisibility(GONE);
         } else {
-          //TODO: set no connection view
+            mLoadingView.setVisibility(GONE);
+            noConnectionView.setVisibility(View.VISIBLE);
         }
         return rootView;
     }
@@ -67,7 +76,10 @@ public class ListArticlesFragment extends Fragment implements LoaderManager.Load
         mLoadingView.hide();
 
         List<Article> articles = (List<Article>) data;
-        mAdapter = new ArticleAdapter(getContext(),articles);
+
+        Log.d(TAG, "onLoadFinished: count articles:" + articles.size());
+
+        mAdapter = new ArticleAdapter(getContext(), articles);
         mRecyclerView.setAdapter(mAdapter);
         LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
         mRecyclerView.setLayoutManager(layoutManager);
--- app/src/main/java/com/example/sasham/goodnews/model/ArticleLoader.java
+++ app/src/main/java/com/example/sasham/goodnews/model/ArticleLoader.java
@@ -4,15 +4,10 @@ import android.content.Context;
 import android.support.v4.content.AsyncTaskLoader;
 import android.util.Log;
 
-import com.example.sasham.goodnews.App;
 import com.example.sasham.goodnews.R;
 import com.example.sasham.goodnews.utils.NetworkUtil;
-import com.example.sasham.goodnews.utils.SharedPreferencesHelper;
 
-import java.io.IOException;
 import java.util.List;
-
-import retrofit2.Response;
 
 /**
  * Created by Sasha M on 14.03.2018.
@@ -35,7 +30,8 @@ public class ArticleLoader extends AsyncTaskLoader<List<Article>> {
     @Override
     public List<Article> loadInBackground() {
         Log.d(TAG, "loadInBackground: ---");
-
-        return NetworkUtil.getTopHeadlines(getContext(),getContext().getResources().getInteger(R.integer.COUNT_TOP_HEADLINES));
+        int count = getContext().getResources().getInteger(R.integer.MAX_COUNT_TOP_HEADLINES);
+        Log.d(TAG, "loadInBackground: count articles:"+count);
+        return NetworkUtil.getTopHeadlines(getContext(), count);
     }
 }
